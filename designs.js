bc0a4001dd152c96d850ae2b1a221ac2e02ce41d
Angela Visnesky <phoenixtat35@yahoo.com>
2018-05-14 09:31:47 -0400

    Add event listener for color change

--- designs.js
+++ designs.js
@@ -1,9 +1,10 @@
+const table = document.getElementById("pixelCanvas");
+const columns = parseInt(document.getElementById("inputWidth").value); //use parseInt to get a number instead of a string
+const rows = parseInt(document.getElementById("inputHeight").value);
+let cellColor = document.getElementById("colorPicker").value;
+
 function makeGrid(event) {
     event.preventDefault(); //prevents default action of form submission
-    const table = document.getElementById("pixelCanvas");
-    const columns = parseInt(document.getElementById("inputWidth").value); //use parseInt to get a number instead of a string
-    const rows = parseInt(document.getElementById("inputHeight").value);
-
     //nested for loop to get number of columns and rows
     for (let row = 0; row < rows; row++) {
         const tableRow = document.createElement("tr");
@@ -16,3 +17,11 @@ function makeGrid(event) {
 }
 
 document.getElementById("sizePicker").addEventListener("submit", makeGrid);
+
+table.addEventListener("click", function(event) {
+    let target = event.target; // where was the click?
+    if (!(target.tagName = 'td')) {
+    } else {
+        target.style.background = cellColor;
+    }
+});
